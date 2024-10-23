fdef531797b20bf1c8d19c4208df9eb7f8bedee3
Jhosus-ui <jhosu.uchiha@gmail.com>
2024-10-23 00:03:31 -0400

    Change de escena
    Agrregado el primer cambio de escena

---
+++ Assets/Scripts/Cambia a escena 2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Cambiaaescena2 : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if colliding with a game object with specific tag
+        if (collision.gameObject.tag.Equals("door1"))
+        {
+            Debug.Log("change scene");
+            SceneManager.LoadScene(1);
+        }
+        //void OntriggerEnter2D(Collider2D other)
+        //{
+        //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //SceneManager.LoadScene(currentSceneIndex + 1);
+        //}
+        //public void LoadSameScene()
+        //{
+        //Scene scene = SceneManager.GetActiveScene();
+        //SceneManager.LoadScene(scene.name);
+        //}
+    }
+}
--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -38,6 +39,15 @@ public class PlayerController : MonoBehaviour
     {
         LeonRb.MovePosition(LeonRb.position + moveInput * speed * Time.fixedDeltaTime);
     }
- 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if colliding with a game object with specific tag
+        if (collision.gameObject.tag.Equals("door1"))
+        {
+            Debug.Log("change scene");
+            SceneManager.LoadScene(1);
+
+        }
+    }
 }
